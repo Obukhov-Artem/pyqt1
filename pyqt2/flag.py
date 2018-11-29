6edfdcd544eee90b9be21da9ba9c757849a26f87
Artem <test@test.ru>
2018-11-29 17:05:38 +0300

    update pyqt22

--- pyqt2/flag.py
+++ pyqt2/flag.py
@@ -1,24 +1,33 @@
 import sys
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from flag_ui import Ui_Form
 
 
-class MyWidget(QMainWindow, Ui_Form):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('my.ui', self)
         self.pushButton.clicked.connect(self.run)
 
     def run(self):
         if self.radioButton.isChecked():
-            self.widget.clear()
-            self.widget.plot([i for i in range(10)], [i for i in range(10)], pen='r')
-        elif self.radioButton_2.isChecked():
-            self.widget.clear()
-            self.widget.plot([i for i in range(10)], [i ** 2 for i in range(10)], pen='g')
-        elif self.radioButton_3.isChecked():
-            self.widget.clear()
-            self.widget.plot([i for i in range(10)], [i ** 3 for i in range(10)], pen='b')
+            self.label.setStyleSheet("QLabel { background-color : red}")
+        if self.radioButton_2.isChecked():
+            self.label.setStyleSheet("QLabel { background-color : blue}")
+        if self.radioButton_3.isChecked():
+            self.label.setStyleSheet("QLabel { background-color : green}")
+        if self.radioButton_6.isChecked():
+            self.label_2.setStyleSheet("QLabel { background-color : red}")
+        if self.radioButton_4.isChecked():
+            self.label_2.setStyleSheet("QLabel { background-color : blue}")
+        if self.radioButton_5.isChecked():
+            self.label_2.setStyleSheet("QLabel { background-color : green}")
+        if self.radioButton_9.isChecked():
+            self.label_3.setStyleSheet("QLabel { background-color : red}")
+        if self.radioButton_7.isChecked():
+            self.label_3.setStyleSheet("QLabel { background-color : blue}")
+        if self.radioButton_8.isChecked():
+            self.label_3.setStyleSheet("QLabel { background-color : green}")
 
 
 app = QApplication(sys.argv)
